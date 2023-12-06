8a36c7fd5eb5ab070fd746eeb492305a4d488386
Yariv Gilad <yariv.gilad@gmail.com>
2023-12-06 12:12:47 +0200

    implemented advanced slug and tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -9,4 +9,33 @@ describe('slugger', () => {
         expect(actual).toBe(expected);
     });
 
+    
+    test('returns a slug from two strings', () => {
+        const actual = slugger('Hello World', 'Goodbye World');
+        const expected = 'hello-world-goodbye-world';
+        expect(actual).toBe(expected);
+    });
+
+    test('returns a slug from three strings', () => {
+        const actual = slugger('Hello World', 'Goodbye World', 'Hello Again');
+        const expected = 'hello-world-goodbye-world-hello-again';
+        expect(actual).toBe(expected);
+    });
+
+    test('returns a slug from three strings with multiple spaces', () => {
+        const actual = slugger(' Hello World ', ' Goodbye  World ', ' Hello    Again ');
+        const expected = 'hello-world-goodbye-world-hello-again';
+        expect(actual).toBe(expected);
+    });
+
 });
+
+
+    
+
+    /* 
+     */
+     
+    /* 
+    */
+    
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 
-export function slugger(...strings : string[]):string{
+/* export function slugger(...strings : string[]):string{
     return strings.join('-')
                   .trim()
                   .toLowerCase()
                   .replaceAll(' ','-');
-}
+} */
+
+
+export function slugger(...strings : string[]):string{
+    return strings.map(str => {
+        return str.trim().toLowerCase().replace(/\s+/g, '-');
+        // Removes multiple spaces with single dash
+    }).join('-');
+} 
